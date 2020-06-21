48ff3dd4c8c77db6424ac2090ae34bd7a1042c19
jalal Arif <jalalharfjalal@gmail.com>
2020-06-21 19:04:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here
+
+class Breakfast{
+    constructor(food,drink){
+      
+    }
+  
+}
+
+
+class Lunch{
+    constructor(salad,soup,drink){
+      
+    }
+  
+}
+
+class Dinner{
+    constructor(salad,soup,entree,dessert){
+      
+    }
+  
+}
